578dd2acf993a49923ec0a0a8da811c3edfbcc36
Ester da Rosa Mariani <darosaester97@gmail.com>
2025-04-09 11:40:17 -0300

    Update Exercicio8.java

--- src/main/java/com/mycompany/exercicio8/Exercicio8.java
+++ src/main/java/com/mycompany/exercicio8/Exercicio8.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.exercicio8;
 
-/**
- *
- * @author daros
- */
 import javax.swing.JOptionPane;
 public class Exercicio8 {
 
